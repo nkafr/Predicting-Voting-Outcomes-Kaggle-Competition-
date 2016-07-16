9eb87e969d2f24d008a9c8e3f934efff421245db
nkafritsas <nik.kafr@gmail.com>
2016-07-16 23:52:15 +0300

    add tuning functionality

--- RandomForest.R
+++ RandomForest.R
@@ -1,7 +1,20 @@
 #install.packages("randomForest")
 library(randomForest)
+#install.packages("caret")
+library(caret)
 
-#Run logistic regression using the most important features (in terms of p-values))
+#tune random forest to find the best parameters (better approach)
+rf_model<-train(Party ~YOB+ Income+ EducationLevel+ HouseholdStatus+ Q109244+ Q115611+ Q98197 +Q113181 +Q98869 +Q101163 + Q99480 +Q105840 +Q120379 + Q116881+ +Q106272+ Q120472 + Q115899 
+                + Q102089 +Q110740 + Q119851 +Q121699 +Q115195 + Q106042 +Q118232 + Q100680 + Q118892 +Q107869, data=train ,method="rf",trControl=trainControl(method="cv",number=5),
+                prox=TRUE,allowParallel=TRUE)
+
+print(rf_model)
+print(rf_model$finalModel)
+
+#alternatively use tune.rf 
+tune.rf <- tuneRF(train[,-7],train[,7], improve=0.05, stepFactor=0.5)
+
+#Execute random forest algorith using the most important features (in terms of p-values))
 forest = randomForest(Party ~YOB+ Income+ EducationLevel+ HouseholdStatus+ Q109244+ Q115611+ Q98197 +Q113181 +Q98869 +Q101163 + Q99480 +Q105840 +Q120379 + Q116881+ +Q106272+ Q120472 + Q115899 + Q102089 +Q110740 + Q119851 +Q121699 +Q115195 + Q106042 +Q118232 + Q100680 + Q118892 +Q107869 , ntree=800, nodesize=30, data=train)
 
 
@@ -10,11 +23,12 @@ forest = randomForest(Party ~YOB+ Income+ EducationLevel+ HouseholdStatus+ Q1092
 #Therefore, one way to measure the importance of a variable is to average the reduction in impurity, 
 #taken over all the times that variable is selected for splitting in all of the trees in the forest
 
-varImpPlot(forest) #or forest$importance[order(-forest$importance), , drop = FALSE]
+varImpPlot(forest) #or forest$importance[order(-forest$importance), , drop = FALSE] or importance(forest)
 #forest$importance conatains the same of varImpPlot(forest) but unordered
 
 
-#Another metric that we can look at is the number of times, aggregated over all of the trees in the random forest model, that a certain variable is selected for a split
+#Another metric that we can look at is the number of times, aggregated over all of the trees in the random forest model, 
+#that a certain variable is selected for a split
 vu = varUsed(forest, count=TRUE)
 vusorted = sort(vu, decreasing = FALSE, index.return = TRUE)
 dotchart(vusorted$x, names(forest$forest$xlevels[vusorted$ix]))
